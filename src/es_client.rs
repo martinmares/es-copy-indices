70820b6621a2e61698107de273a3dad27f7cd598
Martin Mareš <mares@datalite.cz>
2024-04-21 13:44:06 +0200

    Refactor basic_auth -> fn inject_auth

--- src/es_client.rs
+++ src/es_client.rs
@@ -5,7 +5,7 @@ use log::{debug, error, info, warn};
 use rustls::client;
 use std::fmt::Error;
 
-use reqwest::{Certificate, Client, ClientBuilder};
+use reqwest::{Certificate, Client, ClientBuilder, RequestBuilder};
 use tokio::fs::File;
 use tokio::io::AsyncReadExt; // for read_to_end()
 
@@ -13,6 +13,14 @@ use tokio::io::AsyncReadExt; // for read_to_end()
 pub struct EsClient {
     endpoint: Endpoint,
     http_client: Client,
+}
+
+fn inject_auth(request_builder: RequestBuilder, endpoint: Endpoint) -> RequestBuilder {
+    if endpoint.has_basic_auth() {
+        request_builder.basic_auth(endpoint.get_username(), endpoint.get_password())
+    } else {
+        request_builder
+    }
 }
 
 impl EsClient {
@@ -24,15 +32,14 @@ impl EsClient {
             http_client,
         }
     }
+
     async fn call_get(self, path: &str) -> Option<String> {
         let mut request_builder =
             self.http_client
                 .get(format!("{}{}", self.endpoint.get_url(), path));
 
-        if self.endpoint.has_basic_auth() {
-            request_builder = request_builder
-                .basic_auth(self.endpoint.get_username(), self.endpoint.get_password());
-        }
+        let endpoint = self.endpoint.clone();
+        request_builder = inject_auth(request_builder, endpoint);
 
         let call = request_builder.send().await;
         if let Ok(call) = call {
